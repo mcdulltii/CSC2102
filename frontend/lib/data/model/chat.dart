8923652b5fc7a07d48529f28c5696e1ede9551f6
huilunang <huilun2000@gmail.com>
2024-03-04 03:38:15 +0800

    Edit model schema

--- frontend/lib/data/model/chat.dart
+++ frontend/lib/data/model/chat.dart
@@ -1,7 +1,13 @@
 class Chat {
   final String id;
+  final String userId;
   final String title;
   final DateTime timestamp;
 
-  Chat({required this.id, required this.title, required this.timestamp});
+  Chat({
+    required this.id,
+    this.userId = "",
+    required this.title,
+    required this.timestamp,
+  });
 }
--- frontend/lib/data/model/message.dart
+++ frontend/lib/data/model/message.dart
@@ -1,9 +1,11 @@
 class Message {
+  final String chatId;
   final bool isBot;
   final String payload;
   final DateTime timestamp;
 
   const Message({
+    this.chatId = "",
     required this.isBot,
     required this.payload,
     required this.timestamp,
